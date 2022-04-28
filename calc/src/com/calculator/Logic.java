60268f3bacfbfe4fb9c12ef9692d3f4edaadeb82
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2022-04-28 12:36:59 +0300

    vers1

--- calc/src/com/calculator/Logic.java
+++ calc/src/com/calculator/Logic.java
@@ -1,8 +1,14 @@
 package com.calculator;
 
 public class Logic {
-    public static int get_result(int first, int second, String action) {
-        int result = first+second;
+    public static int get_result(int first, int second, char action) {
+        int result=0;
+        switch (action) {
+            case '+' -> result = first + second;
+            case '-' -> result = first - second;
+            case '*' -> result = first * second;
+            case '/' -> result = first / second;
+        }
 
         return result;
     }
--- calc/src/com/calculator/Main.java
+++ calc/src/com/calculator/Main.java
@@ -5,10 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+
         int first= Integer.parseInt(myObj.nextLine());
-        String action= myObj.nextLine();
+        char action= myObj.nextLine().charAt(0);
         int second= Integer.parseInt(myObj.nextLine());
         int result=Logic.get_result(first, second, action);
+
         System.out.println(result);
     }
 }
