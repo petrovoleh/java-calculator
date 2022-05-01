0fb1b50cf207e96a2a829ea05829daf101e8d78e
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2022-05-01 11:28:38 +0300

    123

--- calc/src/com/calculator/GUI.java
+++ calc/src/com/calculator/GUI.java
@@ -1,21 +1,85 @@
 package com.calculator;
 import javax.swing.*;
-import java.awt.Font;
+import java.awt.*;
 
 class numButton extends JButton{
-    Font BigFontTR = new Font("TimesRoman", Font.BOLD, 30);
-    public numButton(String name, int x, int y) {
+    Font ButtonsFont = new Font("TimesRoman", Font.BOLD, 30);
+    public numButton(String name) {
         setText(name);
-        setSize(80, 80);
-        setLocation(x, y);
-        setFont(BigFontTR);
+        setFont(ButtonsFont);
+        setForeground(Color.WHITE);
+        setBackground(Color.DARK_GRAY);
     }
 }
 class Layout extends JFrame {
+    Font TextFont = new Font("TimesRoman", Font.BOLD, 30);
+
     public Layout() {
     setTitle("Calculator");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    getContentPane().setBackground(Color.BLACK);
+
+
+    JPanel grid = new JPanel(new GridBagLayout());
+
+    JLabel output = new JLabel("Some calculations");
+    output.setForeground(Color.WHITE);
+    output.setFont(TextFont);
+    GridBagConstraints constraints = new GridBagConstraints();
+
+
+
+    constraints.ipady     = 50;
+
+    constraints.gridwidth = 4;
+    grid.add(output,constraints);
+
+    constraints.ipadx     = 50;
+    constraints.gridy = 1;
+    constraints.gridwidth = 1;
+
+    grid.add(new numButton("⌫"),constraints);
+    grid.add(new numButton("C"),constraints);
+    grid.add(new numButton("%"),constraints);
+    grid.add(new numButton("/"),constraints);
+
+    constraints.gridy = 2;
+
+    grid.add(new numButton("7"),constraints);
+    grid.add(new numButton("8"),constraints);
+    grid.add(new numButton("9"),constraints);
+    grid.add(new numButton("×"),constraints);
+
+    constraints.gridy = 3;
+
+    grid.add(new numButton("4"),constraints);
+    grid.add(new numButton("5"),constraints);
+    grid.add(new numButton("6"),constraints);
+    grid.add(new numButton("-"),constraints);
+
+    constraints.gridy = 4;
+
+    grid.add(new numButton("1"),constraints);
+    grid.add(new numButton("2"),constraints);
+    grid.add(new numButton("3"),constraints);
+    grid.add(new numButton("+"),constraints);
+
+    constraints.gridy = 5;
+
+    grid.add(new numButton("±"),constraints);
+    grid.add(new numButton("0"),constraints);
+    grid.add(new numButton("."),constraints);
+    grid.add(new numButton("="),constraints);
+
+
+
+    pack();
     setSize(420,640);
+
+    setLayout(new FlowLayout(FlowLayout.RIGHT));
+    add(grid);
+
+    setVisible(true);
 
     }
 
@@ -26,33 +90,7 @@ public class GUI {
 
 
 
-        frame.add(new numButton("<", 10, 110));
-        frame.add(new numButton("C", 110, 110));
-        frame.add(new numButton("%", 210, 110));
-        frame.add(new numButton("/", 310, 110));
 
-        frame.add(new numButton("7", 10, 210));
-        frame.add(new numButton("8", 110, 210));
-        frame.add(new numButton("9", 210, 210));
-        frame.add(new numButton("X", 310, 210));
-
-        frame.add(new numButton("4", 10, 310));
-        frame.add(new numButton("5", 110, 310));
-        frame.add(new numButton("6", 210, 310));
-        frame.add(new numButton("-", 310, 310));
-
-        frame.add(new numButton("1", 10, 410));
-        frame.add(new numButton("2", 110, 410));
-        frame.add(new numButton("3", 210, 410));
-        frame.add(new numButton("+", 310, 410));
-
-        frame.add(new numButton("+/-", 10, 510));
-        frame.add(new numButton("0", 110, 510));
-        frame.add(new numButton(".", 210, 510));
-        frame.add(new numButton("=", 310, 510));
-
-        frame.setLayout(null);
-        frame.setVisible(true);
     }
 }
 
